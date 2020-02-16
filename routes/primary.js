686959b8f88610e75b4b1a52576f5fb1b6700960
trichards140 <44330970+trichards140@users.noreply.github.com>
2020-02-16 12:21:41 -0500

    more fixes

--- routes/primary.js
+++ routes/primary.js
@@ -37,7 +37,7 @@ var corsOptions = {
   },
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors(corsOptions));
+router.use(cors(corsOptions));
 router.use(cookieParser());
 
 // Check For User Auth - If The request makes it past this point, it contains a valid authorization
