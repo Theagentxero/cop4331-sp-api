ac94f9a556602f51575432f4188dca0f71b7b02f
trichards140 <44330970+trichards140@users.noreply.github.com>
2020-02-16 12:46:25 -0500

    allow more methods

--- routes/auth.js
+++ routes/auth.js
@@ -39,6 +39,7 @@ router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://crabrr.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 router.use(cookieParser());
