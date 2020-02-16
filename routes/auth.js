4bc0be89a746c8e9a2e806582e3b06832e56c53a
trichards140 <44330970+trichards140@users.noreply.github.com>
2020-02-16 12:59:02 -0500

    test cookies

--- routes/auth.js
+++ routes/auth.js
@@ -305,6 +305,7 @@ router.get('/logout.json', function(req, res){
     var {timer, result} = initializeRoute(req);
 
     result.setStatus(200);
+    console.log(req.cookies);
     res.clearCookie('jwt', { path: '/' });
     res.clearCookie('userinfo', { path: '/' });
     res.status(result.getStatus()).type('application/json').send(result.getPayload());
